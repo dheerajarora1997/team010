e81951f3b87e915089b3de2a151faeb1d24965f3
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2023-05-29 23:15:33 +0530

    Update main_header.php

--- application/views/pages/main_header.php
+++ application/views/pages/main_header.php
@@ -23,6 +23,6 @@
         <link href="<?= assets ;?>css/jquery-ui.css" rel="stylesheet">
 
 <!-- Font awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" /> -->
 	<!-- Custom styles for this template -->
 </head>
